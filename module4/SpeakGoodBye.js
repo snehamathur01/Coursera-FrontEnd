08444c86c7833284114f0378455c2cc66d7c6c10
snehamathur01 <snehamathur1999@gmail.com>
2020-06-15 18:15:14 +0530

    module 4 solution

---
+++ module4/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function(window) {
+
+  var speakWord ="Good bye"
+  var byeSpeaker= function (name) {
+  console.log(speakWord + " " + name);
+
+}
+ window.byeSpeaker=byeSpeaker;
+
+})(window);
+
